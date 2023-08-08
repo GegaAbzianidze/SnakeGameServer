eee6deb36ceeb83c7c6efb870edef2a8bdd7ce9c
Gega <gega.abzianidze.1@btu.edu.ge>
2023-08-08 17:39:43 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://192.168.1.13:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`a user connected: ${socket.id}`);
+
+  socket.on("join room", (room) => {
+    socket.join(room);
+    console.log(`user ${socket.id} joined room ${room}`);
+    socket.to(room).emit("connectSecondDevice", { id: socket.id, room: room });
+  });
+
+  socket.on("Inputs", (data) => {
+    socket.to(data.room).emit("move", data.input);
+    console.log(
+      `user ${socket.id} sent ${data.input} input to room ${data.room}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("listening on *:5173");
+});
